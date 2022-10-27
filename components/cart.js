02d0c764751fdc48fc5748d70414c48e791de81b
patdel0 <zefiru@protonmail.com>
2022-10-27 16:55:06 +0100

    Add price and linked data
    Co-authored-by: Abdullah <flare-s@users.noreply.github.com>
    Co-authored-by: Joe-Lindie <Joe-Lindie@users.noreply.github.com>

--- components/cart.js
+++ components/cart.js
@@ -1,41 +1,44 @@
 import styles from "./cart.module.css";
-
-const quantity = 5;
-const selectedBooks = [
-  {
-    title: "The No-Show",
-    author: "Beth O'Leary",
-    price: 1,
-  },
-  {
-    title: "The Course of True Love (and First Dates)",
-    author: "Cassandra Clare",
-    price: 8,
-  },
-  {
-    title: "We Need to Talk About Kevin",
-    author: "Lionel Shriver",
-    price: 1,
-  },
-];
-
-export default function Cart() {
+import { useState } from "react";
+import Cartbtn from "./cartbtn";
+export default function Cart({ cart }) {
+  const [displayCart, setDisplayCart] = useState(false);
   return (
     <>
-      <section className={styles.cart}>
-        <h3 className={styles["title"]}>Checkout Items</h3>
-        <ul>
-          {selectedBooks.map((selectedBook) => (
-            <li className={styles.item} key={selectedBook.title}>
-              <span>{selectedBook.title}</span>
-              <span>{selectedBook.author}</span>
-              <span>{selectedBook.price}</span>
-              <span>{quantity}</span>
-            </li>
-          ))}
-        </ul>
-        <button className={styles.button}>Checkout</button>
-      </section>
+      <Cartbtn setDisplayCart={setDisplayCart} />
+      {displayCart && (
+        <section className={styles.cart}>
+          <h3 className={styles["title"]}>Checkout Items</h3>
+          {cart.length > 0 ? (
+            <>
+              <ul>
+                {cart.map((selectedBook) => (
+                  <li className={styles.item} key={selectedBook.title}>
+                    <span>{selectedBook.title}</span>
+                    <span>{selectedBook.author}</span>
+                    <span>price: £{selectedBook.price}</span>
+                    <span>quantity: {selectedBook.quantity}</span>
+                    <span>{`${selectedBook.quantity} x £${
+                      selectedBook.price
+                    } = £${selectedBook.quantity * selectedBook.price} `}</span>
+                  </li>
+                ))}
+              </ul>
+              <div>
+                total: £
+                {cart.reduce(
+                  (sum, current) => (sum += current.quantity * current.price),
+                  0
+                )}
+              </div>
+            </>
+          ) : (
+            <p>Shopping cart is empty...</p>
+          )}
+
+          <button className={styles.button}>Checkout</button>
+        </section>
+      )}
     </>
   );
 }
