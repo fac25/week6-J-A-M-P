1956614ad8f56d97693c9962d32ee47c0a765d1c
Patrick Delaney <zefiru@protonmail.com>
2022-10-27 16:22:46 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,8 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const savedCart = localStorage.getItem('shoppingBasket')
-
-    console.log(JSON.parse(savedCart))
   }, [])
 
-  // const [cart, setCart] = useState([])
-
-  // check if localStorage has shoppingCart
-  // if yes = useState get initial value from localStorage
-  // if not set to empty array
   return <Component {...pageProps} cart={cart} setCart={setCart} />;
 }
 
