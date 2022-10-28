a29c4b34531b89773b389d22d3e0d347837b6990
flare-s <webflarez@gmail.com>
2022-10-28 11:28:00 +0100

    Link around the card

--- components/card.js
+++ components/card.js
@@ -5,62 +5,57 @@ import styles from "../styles/Home.module.css";
 import cardStyles from "./card.module.css";
 
 export default function Card({ book, home, cart, setCart }) {
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const { title, author, genre, id, price, blurb } = book;
   const path = title.replaceAll(" ", "-").toLowerCase();
-  return (
-    <>
-      <section
-        className={`${styles.card} ${cardStyles.card} ${
-          !home && cardStyles.single
-        }`}
-      >
-        {home ? (
-          <Link href={`/books/${id}`}>
-            <Image src={book.src} width={100} height={100} />
-          </Link>
-        ) : (
-          <Image src={`/images/east-of-eden.jpg`} width={100} height={100} />
-        )}
+  const content = (
+    <section
+      className={`${styles.card} ${cardStyles.card} ${
+        !home && cardStyles.single
+      }`}
+    >
+      <Image src={book.src} width={100} height={100} />
 
-        <h2>{title}</h2>
-        <p>
-          <span>Author:</span> {author}
-        </p>
-        <p>
-          <span>Genre:</span> {genre}
-        </p>
-        <p>
-          <span>Price:</span> {price}
-        </p>
-        <div>
-          <span>Quantity: </span>
-          <div className={cardStyles["card-quantity-container"]}>
-            <button
-              className={cardStyles["card-button"]}
-              onClick={() => setQuantity(quantity + 1)}
-            >
-              +
-            </button>
-            <p>{quantity}</p>
-            {/* {<input id="amount" onChange={(e) => setQuantity(Number(e.target.value))<} value={quantity} min="0" type="number" />} */}
-            <button
-              className={cardStyles["card-button"]}
-              onClick={() => quantity > 0 && setQuantity(quantity - 1)}
-            >
-              -
-            </button>
-          </div>
+      <h2>{title}</h2>
+      <p>
+        <span>Author:</span> {author}
+      </p>
+      <p>
+        <span>Genre:</span> {genre}
+      </p>
+      <p>
+        <span>Price:</span> {price}
+      </p>
+      <div>
+        <span>Quantity: </span>
+        <div className={cardStyles["card-quantity-container"]}>
           <button
             className={cardStyles["card-button"]}
-            onClick={() => addToCart({ cart, setCart, title, quantity, book })}
+            onClick={() => setQuantity(quantity + 1)}
           >
-            Add to Basket
+            +
+          </button>
+          <p>{quantity}</p>
+          {/* {<input id="amount" onChange={(e) => setQuantity(Number(e.target.value))<} value={quantity} min="0" type="number" />} */}
+          <button
+            className={cardStyles["card-button"]}
+            onClick={() => quantity > 1 && setQuantity(quantity - 1)}
+          >
+            -
           </button>
         </div>
-        {!home && <p>description: {blurb}</p>}
-      </section>
-    </>
+        <button
+          className={cardStyles["card-button"]}
+          onClick={() => addToCart({ cart, setCart, title, quantity, book })}
+        >
+          Add to Basket
+        </button>
+      </div>
+      {!home && <p>description: {blurb}</p>}
+    </section>
+  );
+  return (
+    <>{home ? <Link href={`/books/${book.id}`}>{content}</Link> : content}</>
   );
 }
 
